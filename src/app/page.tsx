f02981efd9b48434434b8d47535572731b658193
Thiago Pereira <thiago.spereira02@gmail.com>
2025-11-11 13:43:11 +0000

    chore: initialize Next.js 14 project with Tailwind and shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="npx shadcn@latest add button card input dropdown-menu navigation-menu
+">
+    <Button>Funcionou</Button>
+  </main>
+  );
+}
